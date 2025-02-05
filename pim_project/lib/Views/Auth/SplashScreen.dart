96fad9786274bebcae8b3e87e0de351dfa2eb5da
amiragharb <101110249+amiragharb@users.noreply.github.com>
2025-02-05 16:10:21 +0100

    pimproject

---
+++ pim_project/lib/Views/Auth/SplashScreen.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => SplashScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Image.asset(
+          'assets/splash_image.png',
+          width: 250,
+          height: 400,
+        ),
+      ),
+    );
+  }
+}
